276feacf866e08b76ace95e4b5e3bb52b24c473e
MapmakerLich <32036274+MapmakerLich@users.noreply.github.com>
2017-10-20 20:42:07 +0400

    Обновление из-за замечаний
    Причина изначального пути(с указанием папки задания) - работа велась через папку с основным файлом CMakeLists (workshop1), поскольку был риск непрохождения компиляции

--- workshop1/workshop1.3/main.cpp
+++ workshop1/workshop1.3/main.cpp
@@ -7,14 +7,14 @@ int main()
     constexpr unsigned WINDOW_WIDTH = 800;
     constexpr unsigned WINDOW_HEIGHT = 600;
 
-    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Cat");
+    sf::RenderWindow window( sf::VideoMode( WINDOW_WIDTH, WINDOW_HEIGHT ), "Cat" );
 
     sf::Texture texture;
-    texture.loadFromFile("./cat.png");
+    texture.loadFromFile( "./cat.png" );
 
     sf::Sprite sprite;
     sprite.setTexture(texture);
-    sprite.setPosition(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
+    sprite.setPosition( WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 );
 
     while (window.isOpen())
     {
@@ -27,7 +27,7 @@ int main()
             }
         }
 
-        window.clear(sf::Color(255, 255, 255));
+        window.clear(sf::Color( 255, 255, 255 ));
         window.draw(sprite);
         window.display();
     }
