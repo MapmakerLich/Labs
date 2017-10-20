46749f7c3c101e0e2d62629ef9880fc5cd5b17d1
MapmakerLich <32036274+MapmakerLich@users.noreply.github.com>
2017-10-20 20:39:06 +0400

    Update main.cpp

--- workshop1/workshop1.3/main.cpp
+++ workshop1/workshop1.3/main.cpp
@@ -10,7 +10,7 @@ int main()
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Cat");
 
     sf::Texture texture;
-    texture.loadFromFile("./workshop1.3/cat.png");
+    texture.loadFromFile("./cat.png");
 
     sf::Sprite sprite;
     sprite.setTexture(texture);
