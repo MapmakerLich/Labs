24b1ba047cb6b1a1a81cafa7f2a48f388ad61880
MapmakerLich <32036274+MapmakerLich@users.noreply.github.com>
2017-11-09 09:23:11 +0300

    update
    issue update

--- workshop2/workshop2.1/main.cpp
+++ workshop2/workshop2.1/main.cpp
@@ -27,19 +27,19 @@ void update(Ball (&balls)[5], const float deltaTime)
         balls[i].position = balls[i].ball.getPosition();
         balls[i].position += balls[i].speed * deltaTime;
 
-        if ((balls[i].position.x + 80 >= 800)&&(balls[i].speed.x>0))
+        if ((balls[i].position.x + 80 >= 800) && (balls[i].speed.x > 0))
         {
             balls[i].speed.x = - balls[i].speed.x;
         }
-        if ((balls[i].position.x < 0)&&(balls[i].speed.x<0))
+        if ((balls[i].position.x < 0) && (balls[i].speed.x < 0))
         {
             balls[i].speed.x = - balls[i].speed.x;
         }
-        if ((balls[i].position.y + 80 >= 600)&&(balls[i].speed.y>0))
+        if ((balls[i].position.y + 80 >= 600) && (balls[i].speed.y > 0))
         {
             balls[i].speed.y = - balls[i].speed.y;
         }
-        if ((balls[i].position.y < 0)&&(balls[i].speed.y<0))
+        if ((balls[i].position.y < 0) && (balls[i].speed.y < 0))
         {
             balls[i].speed.y = - balls[i].speed.y;
         }
@@ -61,10 +61,10 @@ int main()
     Ball balls[5];
     for (int i = 0; i<std::size(balls); ++i)
     {
-        balls[i].position = {100.f * i+40.f, 100.f};
+        balls[i].position = {100.f * i + 40.f, 100.f};
         balls[i].ball.setPosition(balls[i].position);
-        balls[i].ball.setFillColor(sf::Color(24*i, 50, 150));
-        balls[i].speed = {24.f*i, 120.f};
+        balls[i].ball.setFillColor(sf::Color(24 * i, 50, 150));
+        balls[i].speed = {24.f * i, 120.f};
     }
 
     while (window.isOpen())
